9e1129c381fb9231dd2495a53a7cb4526f933b7f
John Doe <johndoe@example.com>
2022-06-08 21:53:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let arr = [4,16,19,22,11,144,967,19124]
+
+let even = arr.filter(item => item % 2 === 0 )
+let odd = arr.filter(item => item % 2 === 1)
+
+
+console.log(even)
+console.log(odd)
+
+
+
+
+
+
+
+
+
